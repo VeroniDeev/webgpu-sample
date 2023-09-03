8efadcbe981664b1ddcd338c1a2e5ede43a30a46
Veroni <zoubheir@gmail.com>
2023-09-03 16:29:54 +0200

    Add model view projection functionnality

---
+++ src/utils/createTransform.ts
@@ -0,0 +1,27 @@
+import { mat4, vec3 } from "gl-matrix";
+
+const createTransform = (
+  model: mat4,
+  rotation: vec3 = [0, 0, 0],
+  translation: vec3 = [0, 0, 0],
+  scaling: vec3 = [1, 1, 1]
+) => {
+  const rotateXMatrix = mat4.create();
+  const rotateYMatrix = mat4.create();
+  const rotateZMatrix = mat4.create();
+  const translationMatrix = mat4.create();
+  const scalingMatrix = mat4.create();
+
+  mat4.fromXRotation(rotateXMatrix, rotation[0]);
+  mat4.fromXRotation(rotateYMatrix, rotation[1]);
+  mat4.fromXRotation(rotateZMatrix, rotation[2]);
+  mat4.fromTranslation(translationMatrix, translation);
+  mat4.fromScaling(scalingMatrix, scaling);
+
+  mat4.multiply(model, rotateXMatrix, scalingMatrix);
+  mat4.multiply(model, rotateYMatrix, model);
+  mat4.multiply(model, rotateZMatrix, model);
+  mat4.multiply(model, translationMatrix, model);
+};
+
+export default createTransform;
---
+++ src/utils/createViewProjection.ts
@@ -0,0 +1,19 @@
+import { mat4, vec3 } from "gl-matrix";
+
+const createViewProjection = (
+  aspect: number,
+  positionDirection: vec3 = [0, 0, 5],
+  lookDirection: vec3 = [0, 0, 0],
+  upDirection: vec3 = [0, 1, 0]
+) => {
+  const viewProjectionMatrix = mat4.create();
+  const viewMatrix = mat4.create();
+  const projectionMatrix = mat4.create();
+  mat4.lookAt(viewMatrix, positionDirection, lookDirection, upDirection);
+  mat4.perspective(projectionMatrix, Math.PI / 4, aspect, 0.1, 100);
+  mat4.multiply(viewProjectionMatrix, projectionMatrix, viewMatrix);
+
+  return viewProjectionMatrix;
+};
+
+export default createViewProjection;
